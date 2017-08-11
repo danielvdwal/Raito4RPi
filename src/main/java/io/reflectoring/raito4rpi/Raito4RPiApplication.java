8a4c197b103079762cbb0a3246eb45cdf246fa32
danielvdwal <daniel.vanderwal@live.de>
2017-08-11 06:42:53 +0200

    added spring-boot to the project

--- src/main/java/io/reflectoring/raito4rpi/Raito4RPiApplication.java
+++ src/main/java/io/reflectoring/raito4rpi/Raito4RPiApplication.java
@@ -1,9 +1,23 @@
 package io.reflectoring.raito4rpi;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Raito4RPiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Konnichiwa sekai (Hello World)!");
+		SpringApplication.run(Raito4RPiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+		return args -> {
+			System.out.println("Konnichiwa sekai (Hello World)!");
+		};
 	}
 
 }
