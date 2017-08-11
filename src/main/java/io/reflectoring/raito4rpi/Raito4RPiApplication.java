d38776b76852e1c7359f188b23fdec1daf0e8387
danielvdwal <daniel.vanderwal@live.de>
2017-08-11 19:56:48 +0200

    add spring webmvc to the project

--- src/main/java/io/reflectoring/raito4rpi/Raito4RPiApplication.java
+++ src/main/java/io/reflectoring/raito4rpi/Raito4RPiApplication.java
@@ -1,10 +1,7 @@
 package io.reflectoring.raito4rpi;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Raito4RPiApplication {
@@ -12,12 +9,4 @@ public class Raito4RPiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Raito4RPiApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
-		return args -> {
-			System.out.println("Konnichiwa sekai (Hello World)!");
-		};
-	}
-
 }
